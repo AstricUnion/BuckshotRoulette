25b24b694eccb452b25b6e4b9321a8936a9604c5
maxobur0001 <msblock09@gmail.com>
2025-11-06 20:24:12 +0300

    Some improvements in box animation

--- holos/animations.lua
+++ holos/animations.lua
@@ -8,8 +8,28 @@ function animations.getBox(box)
     local tw = Tween:new()
     local position = box:getPos()
     local angles = box:getLocalAngles()
+    box:setLocalAngles(angles + Angle(90, 90, 0))
     tw:add(
-        Param:new(0.1, box, PROPERTY.POS, position + Vector(12, 0, 0):getRotated(angles), math.easeInCubic)
+        Param:new(0.25, box, PROPERTY.POS, position + Vector(12, 0, 0):getRotated(angles), math.easeInCubic)
+    )
+    tw:add(
+        Param:new(0.25, box, PROPERTY.POS, position + Vector(12, 0, 16):getRotated(angles), math.easeInOutCubic)
+    )
+    tw:add(
+        Param:new(0.25, box, PROPERTY.POS, position + Vector(0, 0, 9):getRotated(angles), math.easeInOutCubic),
+        Param:new(0.25, box, PROPERTY.LOCALANGLES, angles, math.easeInOutCubic)
+    )
+    tw:start()
+end
+
+
+function animations.removeBox(box)
+    local tw = Tween:new()
+    local position = box:getPos()
+    local angles = box:getLocalAngles()
+    tw:add(
+        Param:new(0.25, box, PROPERTY.POS, position + Vector(12, 0, 7):getRotated(angles), math.easeInCubic),
+        Param:new(0.25, box, PROPERTY.LOCALANGLES, angles + Angle(90, 90, 0), math.easeInOutCubic)
     )
     tw:add(
         Param:new(0.1, box, PROPERTY.POS, position + Vector(12, -5, 18):getRotated(angles), math.easeInOutCubic),
