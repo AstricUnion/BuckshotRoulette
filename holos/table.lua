132049e805c45154d6e17373628a4a7a1315b3f3
maxobur0001 <msblock09@gmail.com>
2026-04-11 16:23:29 +0300

    fjdkls

--- holos/table.lua
+++ holos/table.lua
@@ -11,13 +11,17 @@ if SERVER then
     ---@class table
     local table = {
         origin = Holo(Rig()),
-        surface = Holo(SubHolo(Vector(0, 0, 20), Angle(), "models/holograms/plane.mdl", Vector(7, 7, 1), nil, nil, nil, "table"))
+        surface = Holo(SubHolo(Vector(0, 0, 20), Angle(), "models/hunter/blocks/cube1x1x1.mdl", Vector(7, 7, 7), nil, nil, nil, "table"))
     }
 
     return table
 else
+    render.createRenderTarget("table")
+
     local mat = material.create("VertexLitGeneric")
     mat:setTextureURL("$basetexture", "https://raw.githubusercontent.com/AstricUnion/BuckshotRoulette/refs/heads/main/textures/table.png")
+    mat:setInt("$flags", 256)
+
     hook.add("HoloInitialized", "table", function(id, holo)
         if id == "table" then
             holo:setSubMaterial(0, "!" .. mat:getName())
